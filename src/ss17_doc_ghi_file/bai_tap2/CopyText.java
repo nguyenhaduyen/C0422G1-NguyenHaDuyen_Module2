d346cbd103702a0a1d3a52ffa775019727ddeb17
nguyenhaduyen <nguyenhaduyen23@gmail.com>
2022-06-28 17:17:29 +0700

    bt

--- src/ss17_doc_ghi_file/bai_tap2/CopyText.java
+++ src/ss17_doc_ghi_file/bai_tap2/CopyText.java
@@ -1,7 +1,17 @@
 package ss17_doc_ghi_file.bai_tap2;
 
-public class CopyText {
-    public  CopyText (String sourceFile, String targetFile) {
+import ss17_doc_ghi_file.bai_tap1.model.Product;
+import ss17_doc_ghi_file.bai_tap1.util.ReadAndWrite;
 
+import java.util.List;
+
+public class CopyText {
+    public static void CopyText (String sourceFile, String targetFile) {
+      List<Product> productList = ReadAndWrite.read(sourceFile);
+      ReadAndWrite.write(targetFile,productList);
+    }
+
+    public static void main(String[] args) {
+         CopyText("src/ss17_doc_ghi_file/bai_tap1/util/product.txt","src/ss17_doc_ghi_file/bai_tap2/file.dat");
     }
 }
